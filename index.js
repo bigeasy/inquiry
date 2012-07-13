6d0dfe95e50870a6ce1632e8b45d9a666924647a
Alan Gutierrez <alan@prettyrobots.com>
2012-07-13 14:29:43 -0400

    Return array always. As function compiler.
    The inquiry function is now a function compiler. It creates a function
    that evaluates teh inquiry expression. The function now always returns
    an array.

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
   else if (typeof define == "function") define(definition);
   else window.inquiry = definition();
 } (function () {
-  var __slice = [].slice;
+  var slice = [].slice;
   function die () {
-    console.log.apply(console, __slice.call(arguments, 0));
+    console.log.apply(console, slice.call(arguments, 0));
     return process.exit(1);
   }
-  function say () { return console.log.apply(console, __slice.call(arguments, 0)) }
+  function say () { return console.log.apply(console, slice.call(arguments, 0)) }
   function error (index) { return "invalid syntax at: " + index }
-  function inquiry (object, query) {
+  function inquiry (query) {
     var i, I, vargs, rest = query, $, index, expression = [];
     while (rest) {
       $ = /^(\s*)(.*)$/.exec(rest), index += $[1].length;
@@ -19,11 +19,14 @@
       expression.push($); 
       rest = $[4];
     }
-    for (i = 0, I = expression.length; i < I; i++) {
-      $ = expression.shift();
-      object = object[$[2]];
+    return function (object) {
+      var vargs = slice.call(arguments, 1);
+      for (i = 0, I = expression.length; i < I; i++) {
+        $ = expression.shift();
+        object = object[$[2]];
+      }
+      return [ object ];
     }
-    return object;
   }
   return inquiry;
 });
