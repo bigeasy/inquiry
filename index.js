6d55547aa1b242f67893436c0a391107a9f7199e
Alan Gutierrez <alan@prettyrobots.com>
2012-07-16 02:04:37 -0400

    Select by wildcard.
    Simple selection by wildcard. We're not yet matching multiple objects,
    populating our array. That will be an issue for another day.
    
    Closes #10.

--- index.js
+++ index.js
@@ -14,16 +14,26 @@
     var i, I, vargs, rest = query, $, index, expression = [];
     while (rest) {
       $ = /^(\s*)(.*)$/.exec(rest), index += $[1].length;
-      $ = /^(\/{1,2})(\w[\w\d]*)(\+?)(.*)/.exec(query);
-      if (!$) throw new Error(error(index));
+      $ = /^(\/{1,2})([\w\*][\*\w\d]*)(\+?)(.*)/.exec(query);
+      if (!$) throw new Error(error(0));
       expression.push($); 
       rest = $[4];
     }
     return function (object) {
-      var vargs = slice.call(arguments, 1);
+      var vargs = slice.call(arguments, 1), star, name, key;
       for (i = 0, I = expression.length; i < I; i++) {
-        $ = expression.shift();
-        object = object[$[2]];
+        $ = expression.shift(), name = $[2];
+        if (~(star = name.indexOf('*'))) {
+          for (key in object) {
+            if (key.indexOf(name.substring(0, star)) == 0
+                && key.lastIndexOf(name.substring(star + 1) == key.length - (name.length - star))) {
+              object = object[key];
+              break;
+            }
+          }
+        } else {
+          object = object[name];
+        }
       }
       return [ object ];
     }
---
+++ t/search/star.t
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+var inquiry = require("../../index");
+require("proof")(3, function (equal) {
+  var object = { name: "George" };
+  equal(inquiry("/n*")(object).pop(), "George", "select by end star");
+  equal(inquiry("/n*e")(object).pop(), "George", "select by middle star");
+  equal(inquiry("/*e")(object).pop(), "George", "select by start star");
+});
