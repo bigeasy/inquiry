eab0a7745406cb007a824efc14cfd51014d46d6c
Alan Gutierrez <alan@prettyrobots.com>
2013-01-02 23:50:07 -0500

    Apply wildcards after an array.
    Apply wildcards to array members. Currently the wildcard name is
    incorrectly applied to the array itself.
    
    Closes #22.

--- t/search/star.t.js
+++ t/search/star.t.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 var inquiry = require("../..");
-require("proof")(3, function (equal) {
+require("proof")(4, function (equal) {
   var object = { name: "George" };
   equal(inquiry("/n*")(object).pop(), "George", "select by end star");
   equal(inquiry("/n*e")(object).pop(), "George", "select by middle star");
   equal(inquiry("/*e")(object).pop(), "George", "select by start star");
+  equal(inquiry('/presidents/l*')(require('./presidents'))[15], 'Lincoln');
 });
