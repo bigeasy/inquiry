939a2dc3c88181e93c0a26e0e50e69525c587b13
Alan Gutierrez <alan@prettyrobots.com>
2013-01-03 23:15:23 -0500

    Implemented sub-queries.
    Implemented sub-queries using square brackets to indicate a sub-query
    predicate. Curly braces are now used to indicate a JavaScript predicate.
    
    The above is a better description and distinction than the ones given in
    the `README.md`. At some point before the next release the headings for
    the `README.md` ought to be changed to **JavaScript Predicates** and
    **Sub-Query Predicates**. StackOverflow has a nice discussion of the
    meaning of the word [predicate in computer
    science](http://stackoverflow.com/questions/3230944/what-does-predicate-mean-in-the-context-of-computer-science).
    
    Closes #15.

--- index.js
+++ index.js
@@ -13,25 +13,47 @@
   function subsexpression (rest, struct) {
     return rest.substring(1);
   }
-  function inquiry (query) {
+  function subquery (test) {
+    return function () {
+      return test.apply(this, arguments).length;
+    }
+  }
+  function parse (query, stop) {
     var i, I, vargs, rest = query, $, index, expression = [], depth = 0, struct, source, args;
     if (query[0] != '/') {
-      rest = '/.' + query;
-      index = -2;
+      if (query[0] != '.') {
+        rest = '/.' + query;
+        index = -2;
+      } else {
+        rest = '/' + query;
+        index = -1;
+      }
     }
-    while (rest) {
+    while (rest && rest[0] != stop) {
+      // Skip leading whitespaces.
       $ = /^(\s*)(.*)$/.exec(rest), index += $[1].length;
-      $ = /^(\/{1,2})(\.|\.\.|[\w\*][\*\w\d]*)(\[?)(.*)/.exec(rest);
+      // Match one or two slashes, followed by dots or a property name, plus an
+      // optional predicate or subquery opener.
+      $ = /^(\/{1,2})(\.|\.\.|[\w\*][\*\w\d]*)([{[]?)(.*)/.exec(rest);
       if (!$) throw new Error(error(0));
       rest = $[4];
       struct = $.slice(1, 3);
-      if ($[3]) {
+      // Check for have a predicate or a sub-expression.
+      switch ($[3]) {
+      // We want to consume the contents of the curly braces that define a
+      // predicate to construct a function body.
+      case "{":
+        // Depth is the number of curly braces we've encountered. We match curly
+        // brances until it is time to pop.
+        // **TODO**: Test against regular expressions. We are going to have to
+        // document the one valid regular expression that we know of that we
+        // cannot match: `/[/]/`.
         depth = 1;
         source = '';
-        while ($ = /^(?:[^'"\[\]]*|'(?:[^\\']|\\.)*'|"(?:[^\\"]|\\.)*")*/.exec(rest)) {
+        while ($ = /^(?:[^'"{}]*|'(?:[^\\']|\\.)*'|"(?:[^\\"]|\\.)*")*/.exec(rest)) {
           source += $[0];
           rest = rest.substring($[0].length);
-          if (rest[0] == ']') {
+          if (rest[0] == '}') {
             break;
           }
         }
@@ -46,22 +68,29 @@
         }
         args.push('return ' + source);
         struct.push(Function.apply(Function, args));
-      } else {
+        break;
+      // We want to consume the contents of brackets as a sub-expression, so we
+      // call ourselves recursively.
+      case "[":
+        var sub = parse(rest, "]");
+        struct.push(subquery(sub.shift()));
+        rest = sub.shift().slice(1);
+        break;
+      default:
         struct.push(null);
       }
       expression.push(struct);
     }
     function subexpression (test, object, vargs) {
-      if (test.length) {
-        return test.apply(this, [ object ].concat(vargs.slice(0, test.length - 1)));
+      if (true || test.length) {
+        return test.apply(object, [ object ].concat(vargs.slice(0, test.length - 1)));
       }
       return true;
     }
-    return function (object) {
-      var vargs = slice.call(arguments, 1)
-        , candidates = [], candidate, stack = [ object ]
-        , star, name, key, i, I, test
-        ;
+    return [ function (object) {
+      var vargs = slice.call(arguments, 1),
+          candidates = [], candidate, stack = [ object ],
+          star, name, key, i, I, test;
       for (i = 0, I = expression.length; i < I; i++) {
         name = expression[i][1], test = expression[i][2];
         while (stack.length) {
@@ -96,7 +125,7 @@
         }
       }
       return stack;
-    }
+    }, rest ];
   }
-  return inquiry;
+  return function (query) { return parse(query).shift() }
 });
