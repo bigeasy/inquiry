2abcdc9d2ec387aab113296a934a189441881479
Alan Gutierrez <alan@prettyrobots.com>
2013-03-09 00:43:13 -0500

    Test that no dot is required in sub-queries.

--- t/search/subqueries.t.js
+++ t/search/subqueries.t.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
 var inquiry = require("../..");
-require("proof")(2, function (equal) {
+require("proof")(4, function (equal) {
   var object;
   object = { firstName: "Abraham", lastName: "Lincoln" };
   equal(inquiry("[.{$.firstName == 'Abraham'}]")(object).pop().lastName, 'Lincoln', 'select by property');
+  equal(inquiry("[{$.firstName == 'Abraham'}]")(object).pop().lastName, 'Lincoln', 'select by property no dot');
   object = require('./presidents');
-  equal(inquiry("/presidents[.{$.firstName == 'Abraham'}]")(object).pop().lastName, 'Lincoln', 'select by property');
+  equal(inquiry("/presidents[.{$.firstName == 'Abraham'}]")(object).pop().lastName, 'Lincoln', 'select by property many');
+  equal(inquiry("/presidents[{$.firstName == 'Abraham'}]")(object).pop().lastName, 'Lincoln', 'select by property many no dot');
 });
