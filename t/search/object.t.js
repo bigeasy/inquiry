b29d0ca3db56b4ae13227bbcf0ce5bab0cd63c0d
Alan Gutierrez <alan@prettyrobots.com>
2013-06-19 23:29:56 -0400

    Test selecting a non-existent property.

--- t/search/object.t.js
+++ t/search/object.t.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 var inquiry = require("../..");
-require("proof")(3, function (equal, ok) {
+require("proof")(4, function (equal, ok) {
   var object = { name: "Alan", zero: 0, "null": null };
   equal(inquiry("/name")(object).pop(), "Alan", "select by name");
   equal(inquiry("/zero")(object).pop(), 0, "select zero");
   ok(inquiry("/null")(object).pop() === null, "select null");
+  equal(inquiry("/missing")(object).length, 0, "select nothing");
 });
