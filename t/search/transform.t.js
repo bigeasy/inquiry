cecea68de54ec050b3f7f6c907588737690069f1
Alan Gutierrez <alan@prettyrobots.com>
2018-07-31 03:10:42 -0500

    Test transforms with implied root slash.

--- t/search/transform.t.js
+++ t/search/transform.t.js
@@ -1,11 +1,11 @@
-require("proof")(3, prove)
+require("proof")(4, prove)
 
 function prove (okay) {
     var inquiry = require("../..")
     var object, result
     object = { firstName: "Abraham", lastName: "Lincoln" }
-//    okay(inquiry('/presidents{ $.lastName + ", " + $.firstName }')(object), 'Lincoln, Abraham', 'transform')
     okay(inquiry('/.>{ $.lastName + ", " + $.firstName }')(object), [ 'Lincoln, Abraham' ], 'transform')
     okay(inquiry('/.>{ $.nothing }')(object), [], 'transform empty')
     okay(inquiry('/.>{ [ $.firstName, $.lastName ] }')(object), [ 'Abraham', 'Lincoln' ], 'transform array')
+    okay(inquiry('>{ [ $.firstName, $.lastName ] }')(object), [ 'Abraham', 'Lincoln' ], 'transform assume root')
 }
