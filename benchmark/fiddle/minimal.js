fdf0e01c7f5395501b8fdbba2cf91573480dddbc
Alan Gutierrez <alan@prettyrobots.com>
2018-07-31 03:16:07 -0500

    Inquiry is slow.
    Unfortuantely, inquiry is quite slow. Much slower than the evaluator I
    was going to use Inquiry to replace.

--- benchmark/fiddle/minimal.js
+++ benchmark/fiddle/minimal.js
@@ -1,15 +1,28 @@
-var inquiry = require('..')
+var inquiry = require('../..')
 var Benchmark = require('benchmark')
 var assert = require('assert')
+var Evaluator = require('prolific.evaluator')
 
 var suite = new Benchmark.Suite('async', { /*minSamples: 100*/ })
 
-var object = require('../t/search/presidents')
+var object = require('../../t/search/presidents')
 
 var q = inquiry('/presidents/3')
 
+var w = inquiry('>{ $.presidents[3] }')
+
+var e = Evaluator.create('$.presidents[3]')
+
 function inq (object) {
     return q(object).shift()
+}
+
+function transform (object) {
+    return w(object).shift()
+}
+
+function evaluator (object) {
+    return e(object)
 }
 
 function raw (object) {
@@ -21,6 +34,20 @@ for (var i = 1; i <= 4; i++)  {
         name: 'inquiry ' + i,
         fn: function () {
             assert(inq(object).lastName, 'Madison')
+        }
+    })
+
+    suite.add({
+        name: 'transform ' + i,
+        fn: function () {
+            assert(transform(object).lastName, 'Madison')
+        }
+    })
+
+    suite.add({
+        name: 'evaluator ' + i,
+        fn: function () {
+            assert(evaluator({ json: object }).lastName, 'Madison')
         }
     })
 
